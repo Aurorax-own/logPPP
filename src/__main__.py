7f0cd8e3d7ffa50deb5b741d3985cd30379e9648
YCJ <15047150695@163.com>
2023-07-26 21:47:13 +0800

    __version__ = '1.0.4',添加输出 是否开启颜色

---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+import logPPP
+
+logPPP.LEVEL = logPPP.logPPPLevel.DEBUG
+logPPP.IS_COLOR = True
+
+logPPP.warning("warning", is_color=False)
+
+logPPP.error("error",is_color=False)
+
+logPPP.debug("debug")

b8af9f5d8acfea8d8719eb747468351c131ffb50
YCJ <15047150695@163.com>
2023-07-27 11:51:04 +0800

    __version__ = '1.1.1',修复参数接收问题

--- src/__main__.py
+++ src/__main__.py
@@ -3,8 +3,9 @@ import logPPP
 logPPP.LEVEL = logPPP.logPPPLevel.DEBUG
 logPPP.IS_COLOR = True
 
-logPPP.warning("warning", is_color=False)
-
-logPPP.error("error",is_color=False)
-
+logPPP.info("info", 'test', is_color=True)
+logPPP.warning("warning")
+logPPP.error("error")
 logPPP.debug("debug")
+logPPP.critical("critical")
+logPPP.critical("critical", is_color=False)
--- src/logPPP/__init__.py
+++ src/logPPP/__init__.py
@@ -5,7 +5,7 @@ from ._util import *
 from .logPPPLevel import logPPPLevel
 
 __all__ = ['__version__', 'IS_COLOR', 'LEVEL', 'logPPPLevel', 'info', 'warning', 'error', 'debug', 'critical']
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 # 是否开启颜色
 IS_COLOR = True
@@ -14,9 +14,10 @@ LEVEL = logPPPLevel.INFO
 
 
 # 日志输出
-def _base(args, sep=' ', end='\n', file=None, _level=logPPPLevel.INFO, color=ConsoleColors.RESET, is_color=True):
+def _base(*args, sep=' ', end='\n', file=None, _level=logPPPLevel.INFO, color=ConsoleColors.RESET, is_color=True):
+    args = sep.join(map(str, args))
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    args = "{:<20} {:<8} {}  {}".format(timestamp, get_caller_file_basename_path(), _level['name'], args)
+    args = "{:<20} {:<8} {}  {}".format(timestamp, get_caller_file_basename_path(), _level.get('name'), args)
     if is_color is None and IS_COLOR or is_color is True:
         args = f"{color}{args}{ConsoleColors.RESET}"
     if _level['level'] >= LEVEL['level']:
@@ -24,25 +25,25 @@ def _base(args, sep=' ', end='\n', file=None, _level=logPPPLevel.INFO, color=Con
 
 
 # info等级
-def info(args, sep=' ', end='\n', file=None, is_color=None):
-    _base(args, sep, end, file, logPPPLevel.INFO, ConsoleColors.RESET, is_color)
+def info(*args, sep=' ', end='\n', file=None, is_color=None):
+    _base(*args, sep=sep, end=end, file=file, _level=logPPPLevel.INFO, color=ConsoleColors.RESET, is_color=is_color)
 
 
 # warning等级
-def warning(args, sep=' ', end='\n', file=None, is_color=None):
-    _base(args, sep, end, file, logPPPLevel.WARNING, ConsoleColors.YELLOW, is_color)
+def warning(*args, sep=' ', end='\n', file=None, is_color=None):
+    _base(*args, sep=sep, end=end, file=file, _level=logPPPLevel.WARNING, color=ConsoleColors.YELLOW, is_color=is_color)
 
 
 # error等级
-def error(args, sep=' ', end='\n', file=None, is_color=None):
-    _base(args, sep, end, file, logPPPLevel.ERROR, ConsoleColors.RED, is_color)
+def error(*args, sep=' ', end='\n', file=None, is_color=None):
+    _base(*args, sep=sep, end=end, file=file, _level=logPPPLevel.ERROR, color=ConsoleColors.RED, is_color=is_color)
 
 
 # debug等级
-def debug(args, sep=' ', end='\n', file=None, is_color=None):
-    _base(args, sep, end, file, logPPPLevel.DEBUG, ConsoleColors.RED, is_color)
+def debug(*args, sep=' ', end='\n', file=None, is_color=None):
+    _base(*args, sep=sep, end=end, file=file, _level=logPPPLevel.DEBUG, color=ConsoleColors.RED, is_color=is_color)
 
 
 # critical等级
-def critical(args, sep=' ', end='\n', file=None, is_color=None):
-    _base(args, sep, end, file, logPPPLevel.CRITICAL, ConsoleColors.RED, is_color)
+def critical(*args, sep=' ', end='\n', file=None, is_color=None):
+    _base(*args, sep=sep, end=end, file=file, _level=logPPPLevel.CRITICAL, color=ConsoleColors.RED, is_color=is_color)

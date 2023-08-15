adafb8191acb2fcb23d6ee0be31d68f46a1ac1f4
Aurorax-own <15047150695@163.cpm>
2023-08-15 16:30:47 +0800

    __version__ = '1.2.0'

--- src/logPPP/__init__.py
+++ src/logPPP/__init__.py
@@ -1,49 +1,79 @@
-import datetime
+import logging
+import os
+import sys
+from logging import *
 
-from .__ConsoleColors__ import ConsoleColors
-from .__Level__ import Level
-from .__util__ import *
+__all__ = ['__version__', 'logger', 'Config', 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL', 'NOTSET', 'FATAL',
+           'WARN']
+__version__ = '1.2.0'
 
-__all__ = ['__version__', 'IS_COLOR', 'LEVEL', 'Level', 'info', 'warning', 'error', 'debug', 'critical']
-__version__ = '1.1.2'
-
-# 是否开启颜色
-IS_COLOR = True
-# 等级
-LEVEL = Level.INFO
-
-
-# 日志输出
-def _base(*args, sep=' ', end='\n', file=None, _level=Level.INFO, color=ConsoleColors.RESET, is_color=True):
-    args = sep.join(map(str, args))
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    args = "{:<20} {:<8} {}  {}".format(timestamp, get_caller_file_basename_path(), _level.get('name'), args)
-    if is_color is None and IS_COLOR or is_color is True:
-        args = f"{color}{args}{ConsoleColors.RESET}"
-    if _level['level'] >= LEVEL['level']:
-        print(args, sep=sep, end=end, file=file)
+_LOGGING_LEVEL = logging.INFO
+_LOGGING_IS_OUTPUT_FILE = True
+_LOGGING_IS_OUTPUT_SYS_STDOUT = True
+_LOGGING_FILE = os.path.join(os.getcwd(), "log.log")
+_LOGGER_NAME = os.path.splitext(os.path.basename(_LOGGING_FILE))[0]
+_LOGGING_FMT = '[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d] - %(message)s'
+_LOGGING_DATE_FMT = '%Y-%m-%d %H:%M:%S'
 
 
-# info等级
-def info(*args, sep=' ', end='\n', file=None, is_color=None):
-    _base(*args, sep=sep, end=end, file=file, _level=Level.INFO, color=ConsoleColors.GREEN, is_color=is_color)
+def _get_logger():
+    log = logging.getLogger(_LOGGER_NAME)
+    for h in log.handlers:
+        h.close()
+        log.removeHandler(h)
+        del h
+    log.handlers.clear()
+    log.propagate = False
+    log.setLevel(_LOGGING_LEVEL)
+
+    if _LOGGING_IS_OUTPUT_SYS_STDOUT:
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setFormatter(logging.Formatter(_LOGGING_FMT, datefmt=_LOGGING_DATE_FMT))
+        log.addHandler(ch)
+
+    if _LOGGING_IS_OUTPUT_FILE:
+        fh = logging.FileHandler(_LOGGING_FILE, encoding='utf-8')
+        fh.setFormatter(logging.Formatter(_LOGGING_FMT, datefmt=_LOGGING_DATE_FMT))
+        log.addHandler(fh)
+    return log
 
 
-# warning等级
-def warning(*args, sep=' ', end='\n', file=None, is_color=None):
-    _base(*args, sep=sep, end=end, file=file, _level=Level.WARNING, color=ConsoleColors.YELLOW, is_color=is_color)
+# 日志句柄
+logger = _get_logger()
 
 
-# error等级
-def error(*args, sep=' ', end='\n', file=None, is_color=None):
-    _base(*args, sep=sep, end=end, file=file, _level=Level.ERROR, color=ConsoleColors.RED, is_color=is_color)
+def _auto_refresh_config(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        global logger
+        logger = _get_logger()
+        return result
+
+    return wrapper
 
 
-# debug等级
-def debug(*args, sep=' ', end='\n', file=None, is_color=None):
-    _base(*args, sep=sep, end=end, file=file, _level=Level.DEBUG, color=ConsoleColors.RED, is_color=is_color)
+@_auto_refresh_config
+def Config(logging_level=None, logging_is_output_file=None, logging_is_output_sys_stdout=None,
+           logging_file=None, logger_name=None, logging_fmt=None, logging_date_fmt=None):
+    global _LOGGING_LEVEL, _LOGGING_IS_OUTPUT_FILE, _LOGGING_IS_OUTPUT_SYS_STDOUT, _LOGGING_FILE, _LOGGER_NAME, _LOGGING_FMT, _LOGGING_DATE_FMT
 
+    if logging_level is not None:
+        _LOGGING_LEVEL = logging_level
 
-# critical等级
-def critical(*args, sep=' ', end='\n', file=None, is_color=None):
-    _base(*args, sep=sep, end=end, file=file, _level=Level.CRITICAL, color=ConsoleColors.RED, is_color=is_color)
+    if logging_is_output_file is not None:
+        _LOGGING_IS_OUTPUT_FILE = logging_is_output_file
+
+    if logging_is_output_sys_stdout is not None:
+        _LOGGING_IS_OUTPUT_SYS_STDOUT = logging_is_output_sys_stdout
+
+    if logging_file is not None:
+        _LOGGING_FILE = logging_file
+
+    if logger_name is not None:
+        _LOGGER_NAME = logger_name
+
+    if logging_fmt is not None:
+        _LOGGING_FMT = logging_fmt
+
+    if logging_date_fmt is not None:
+        _LOGGING_DATE_FMT = logging_date_fmt
--- src/test.py
+++ src/test.py
@@ -1,22 +1,3 @@
-# 日志记录
-
-## 简介
-日志记录模块是一个简单的日志输出工具，它可以将不同等级（INFO、WARNING、ERROR、DEBUG、CRITICAL）的日志输出到控制台。
-
-## 使用方法
-要使用日志记录模块，请在您的Python脚本或模块中导入它，如下所示：
-
-```
-pip install logPPP
-```
-
-```python
-import logPPP/from logPPP import *
-```
-
-## 示例
-
-```python
 from logPPP import *
 
 Config(logging_is_output_sys_stdout=True, logging_is_output_file=False, logging_level=DEBUG)
@@ -34,4 +15,3 @@ logPPP.logger.debug("debug")
 logPPP.logger.warning("warning")
 logPPP.logger.error("error")
 logPPP.logger.critical("critical")
-```
